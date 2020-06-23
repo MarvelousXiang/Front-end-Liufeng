bd05f91ba6f1e1eb0f5faa67260905640a1a43a8
MarvelousXiang <1007251433@qq.com>
2020-06-23 22:32:29 +0800

    feat: debug done

--- micro_app_food-master/miniprogram/app.js
+++ micro_app_food-master/miniprogram/app.js
@@ -1,17 +1,17 @@
 //app.js
-// const ald = require('./utils/ald-stat.js')
+//const ald = require('./utils/ald-stat.js')
 
 App({
   onLaunch: function () {
     
-    if (!wx.cloud) {
+    /*if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: "recipes-17ddbf",//这个就是环境id
+        env: "wxf87d071d97ebf0c9",//这个就是环境id
         traceUser: true,
       })
-    }
+    }*/
     
     this.globalData = {}
   }
